8e99645444fc11771caa573bb66277bbe76a5983
Dant777 <47300885+Dant777@users.noreply.github.com>
2022-08-02 20:20:11 +0300

    Создание класса столов

---
+++ MDA/Entities/Table.cs
@@ -0,0 +1,32 @@
+﻿namespace MDA.Entities
+{
+    internal enum State
+    {
+        Free=0,
+        Booked=1
+    }
+
+    internal sealed class Table
+    {
+        public Table(int id)
+        {
+            Id=id;
+            State = State.Free;
+            SeatsCount = Random.Shared.Next(2, 5);
+        }
+
+        public State State { get; private set; }
+        public int SeatsCount { get; } 
+        public int Id { get; }
+
+        public bool SetState(State state)
+        {
+            if (state == State)
+            {
+                return false;
+            }
+            State = state;
+            return true;
+        }
+    }
+}
