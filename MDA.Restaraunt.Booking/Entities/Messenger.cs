1925d59d933dcaff226a1cd29294d9bc11a1a8f4
Dant777 <47300885+Dant777@users.noreply.github.com>
2022-08-14 08:55:14 +0300

    Перечисление для цветов текста сообщений

--- MDA.Restaraunt.Booking/Entities/Messenger.cs
+++ MDA.Restaraunt.Booking/Entities/Messenger.cs
@@ -2,6 +2,7 @@
 {
     public static class Messenger
     {
+
         public static void PrintTxt(string txt)
         {
             Console.WriteLine(txt + "\n");
---
+++ MDA.Restaraunt.Booking/Entities/MsgColor.cs
@@ -0,0 +1,11 @@
+﻿namespace MDA.Restaraunt.Booking.Entities;
+/// <summary>
+/// Цветовое отображение сообщений
+/// </summary>
+public enum MsgColor
+{
+    Normal = 0,
+    Error = 1,
+    Answer = 2,
+    Info = 3
+}
