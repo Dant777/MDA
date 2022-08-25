92c13d492d3ff44b23755c4df06a0792de462bb5
Dant777 <47300885+Dant777@users.noreply.github.com>
2022-08-25 16:34:24 +0300

    HW-7

--- MDA.Restaraunt.Booking/Program.cs
+++ MDA.Restaraunt.Booking/Program.cs
@@ -1,18 +1,6 @@
-﻿using System.Security.Authentication;
-using MassTransit;
-using MDA.Restaraunt.Booking.Consumers;
-using MDA.Restaraunt.Booking.Entities;
-using MDA.Restaraunt.Messages;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using System.Text;
-using MassTransit.Audit;
-using MDA.Restaraunt.Messages.Repository;
-using MDA.Restaraunt.Booking.Schedules;
-using MDA.Restaraunt.Messages.DbData;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace MDA.Restaraunt.Booking
 {
---
+++ MDA.Tests/RestaurantConsumersTest.cs
@@ -0,0 +1,54 @@
+﻿using MassTransit.Testing;
+using MDA.Restaraunt.Booking.Consumers;
+using MDA.Restaraunt.Booking.Entities;
+using MDA.Restaraunt.Messages;
+using MDA.Restaraunt.Messages.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MDA.Tests
+{
+    [TestFixture]
+    public class RestaurantConsumersTest
+    {
+        private ServiceProvider _provider;
+        private ITestHarness _harness;
+
+        [OneTimeSetUp]
+        public async Task Init()
+        {
+            _provider = new ServiceCollection()
+                .AddMassTransitInMemoryTestHarness(cfg =>
+                {
+                    cfg.AddConsumer<BookingRequestConsumer>();
+                })
+                .AddLogging()
+                .AddTransient<Restaurant>()
+                .AddSingleton<IBookingRequestRepository, BookingRequestRepository>()
+                .BuildServiceProvider(true);
+            _harness = _provider.GetTestHarness();
+            await _harness.Start();
+        }
+
+        [OneTimeTearDown]
+        public async Task TearDown()
+        {
+            await _harness.OutputTimeline(TestContext.Out, options => options.Now().IncludeAddress());
+            await _provider.DisposeAsync();
+        }
+
+        [Test]
+        public async Task Any_booking_request_consumed()
+        {
+            var orderIdd = Guid.NewGuid();
+
+            await _harness.Bus.Publish(
+                (IBookingRequest)new BookingRequest(
+                    orderIdd,
+                    Guid.NewGuid(),
+                    null,
+                    DateTime.Now));
+
+            Assert.That(await _harness.Consumed.Any<IBookingRequest>());
+        }
+    }
+}
